8189c2f013f66d13e57e1616cc85f2ec438bc0a6
Vinodkumar M <vinod.kumar@improve10x.com>
2022-12-29 23:29:08 +0530

    Implemented Fragment Functionality.

---
+++ app/src/main/java/com/improve10x/chatwithv2/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.improve10x.chatwithv2.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.improve10x.chatwithv2.HistoryFragment;
+import com.improve10x.chatwithv2.HomeFragment;
+import com.improve10x.chatwithv2.R;
+import com.improve10x.chatwithv2.TemplatesFragment;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+    private static final String[] TAB_TITLES = new String[]{"Home", "Templates", "History"};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        if (position == 0) {
+            return new HomeFragment();
+        }else if (position == 1) {
+            return  new TemplatesFragment();
+        }else {
+            return new HistoryFragment();
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TAB_TITLES[position];
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return TAB_TITLES.length;
+    }
+}
