e4fc3de094fd36f68c7efabb230b4de8ae1485ea
Harsha <shreeharsha.a@improve10x.com>
2022-12-29 16:27:38 +0530

    Implemented SplashActivity

--- app/src/main/java/com/improve10x/chatwithv2/HomeActivity.java
+++ app/src/main/java/com/improve10x/chatwithv2/HomeActivity.java
@@ -2,15 +2,13 @@ package com.improve10x.chatwithv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
