7f5d0b18f409411ade980bb251d480e0613fa678
Harsha <shreeharsha.a@improve10x.com>
2023-01-02 13:11:16 +0530

    implemented whatsapp functionality

--- app/src/main/java/com/improve10x/chatwithv2/HomeFragment.java
+++ app/src/main/java/com/improve10x/chatwithv2/HomeFragment.java
@@ -9,34 +9,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.improve10x.chatwithv2.databinding.FragmentHomeBinding;
-import com.improve10x.chatwithv2.historyItem.HistoryItem;
-
-import java.util.Random;
 
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding fragmentHomeBinding;
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         fragmentHomeBinding = FragmentHomeBinding.inflate(getLayoutInflater());
-        setUpWhatsAppData();
+        handleWhatsappBtn();
+        handleWhatsappBusinessBtn();
         return fragmentHomeBinding.getRoot();
     }
 
-    private void setUpWhatsAppData() {
+    private void handleWhatsappBtn() {
         fragmentHomeBinding.whatsappBtn.setOnClickListener(view -> {
             String name = fragmentHomeBinding.nameTxt.getText().toString();
             String number = fragmentHomeBinding.numberTxt.getText().toString();
             String message = fragmentHomeBinding.messageTxt.getText().toString();
-            String url = "https://api.whatsapp.com/send?phone=" + name + "&text=" + number + "&text=" + message;
+            String url = "https://api.whatsapp.com/send?phone=" + number + "&text=" + message;
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            i.setPackage("com.whatsapp");
+            startActivity(i);
+        });
+    }
+
+    private void handleWhatsappBusinessBtn() {
+        fragmentHomeBinding.wabusinessBtn.setOnClickListener(view -> {
+            String name = fragmentHomeBinding.nameTxt.getText().toString();
+            String number = fragmentHomeBinding.numberTxt.getText().toString();
+            String message = fragmentHomeBinding.messageTxt.getText().toString();
+            String url = "https://api.whatsapp.com/send?phone=" + number + "&text=" + message;
             Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData(Uri.parse(url));
             i.setPackage("com.whatsapp");
