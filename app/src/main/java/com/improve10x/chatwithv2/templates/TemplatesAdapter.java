c6e6cc849adb590781f04475423ca91ad1d9daae
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-02 11:48:34 +0530

    I added visibilities for delete and add Templates button in Templates fragment

--- app/src/main/java/com/improve10x/chatwithv2/templates/TemplatesAdapter.java
+++ app/src/main/java/com/improve10x/chatwithv2/templates/TemplatesAdapter.java
@@ -37,6 +37,7 @@ public class TemplatesAdapter extends RecyclerView.Adapter<TemplateViewHolder> {
     public void onBindViewHolder(@NonNull TemplateViewHolder holder, int position) {
         Template template = this.templates.get(position);
         holder.binding.messageTextTxt.setText(template.messageText);
+
         if (template.titleText == null || template.titleText.isEmpty()) {
             holder.binding.titleTxt.setVisibility(View.GONE);
         } else {
@@ -45,6 +46,8 @@ public class TemplatesAdapter extends RecyclerView.Adapter<TemplateViewHolder> {
         }
         holder.binding.getRoot().setOnLongClickListener(view -> {
             holder.binding.actionLayout.setVisibility(View.VISIBLE);
+            holder.binding.editBtn.setVisibility(View.VISIBLE);
+            holder.binding.deleteBtn.setVisibility(View.VISIBLE);
             return true;
         });
         holder.binding.deleteBtn.setOnClickListener(view -> {
